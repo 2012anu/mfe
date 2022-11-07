e50bd73bf14d910ee4fd99550fa7cacf5459c678
Anusha <anusha.mysuru2012@gmail.com>
2022-11-07 12:32:10 +0530

    first commit

---
+++ container/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import {mount} from 'marketing/marketing';
+// console.log("JHBDKWHBE",mount);
+import MarketingApp from './components/MarketingApp';
+export default () =>
+{
+    return (
+        <>
+        <div>Hi there!!!</div>
+        <hr></hr>
+
+        <MarketingApp />
+        </>
+    );
+};
